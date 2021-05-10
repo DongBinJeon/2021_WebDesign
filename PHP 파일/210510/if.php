660226f4cdc99513de7d68385e9b11102ec63303
DongBinJeon <2019261@sungil-i.kr>
2021-05-10 20:37:48 +0900

    210510

---
+++ PHP 파일/210510/if.php
@@ -0,0 +1,16 @@
+<?php
+$score = 40;
+
+if($score >= 90){
+    echo "당신의 학점은 A 입니다.<br/>";
+}else if($score >= 80){
+    echo "당신의 학점은 B 입니다.<br/>";
+}else if($score >= 70){
+    echo "당신의 학점은 C 입니다.<br/>";
+}else if($score >= 60){
+    echo "당신의 학점은 D 입니다.<br/>";
+}else{
+    echo "당신의 학점은 E 입니다.<br/>";
+}
+
+?>
---
+++ PHP 파일/210510/switch.php
@@ -0,0 +1,20 @@
+<?php
+    $score = 65;
+
+    switch($score){
+        case 100:
+            echo "당신의 점수는 {$score}입니다.<br/>";
+            break;
+        case 90:
+            echo "당신의 점수는 {$score}입니다.<br/>";
+            break;
+        case 80:
+            echo "당신의 점수는 {$score}입니다.<br/>";
+            break;
+        default:
+            echo "당신의 점수는 {$score}입니다.<br/>";
+            break;
+    }
+
+
+?>
