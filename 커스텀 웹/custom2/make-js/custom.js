bf056220dbcc57df086e29e95a3cfc0a1177b6cf
DongBinJeon <2019261@sungil-i.kr>
2021-10-15 08:40:11 +0900

    211015 update

--- 커스텀 웹/custom2/make-js/custom.js
+++ 커스텀 웹/custom2/make-js/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+    $(".menu > li").on("click", function(){
+        $(this).data('clicked', true);
+
+        if($(".menu > li").data('clicked')){
+            $(this).getElementByClassName();
+        }
+    });
     
+        
+
 });
